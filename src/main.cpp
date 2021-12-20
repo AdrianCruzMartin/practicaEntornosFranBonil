07afa618957506cf72aa587c481ee4a050641fea
Adrian <you@example.com>
2021-12-20 10:06:26 +0100

    hola

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,41 @@ int main()
     int base=0;
     int altura=0;
     int resul;
-
-
+    cout << ":D";
     cout<<"Este programa calcula el area de un triangulo"<<endl;
+    do
+    {
     cout<<"introduce la base del triangulo: "<<endl;
 
     cin>>base;
-
+    } while (base<1 || base>15);
+//hola
+    do
+    {
     cout<<"introduce la altura: "<<endl;
 
     cin>>altura;
+    }while (altura<1 || altura>15);
 
     resul=(base*altura)/2;
 
     cout<<"el area del triangulo es: "<<resul;
     
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
 
     return 0;
 }
